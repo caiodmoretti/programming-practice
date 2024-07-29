fe63c021abe7c6024c15d2771266c8fc4a98943c
Caio <caiodmoretti@gmail.com>
2024-07-29 15:40:02 -0300

    Nested loops and divisibility

---
+++ Algorithm Problems/src/NumnerOfGoodPairs.java
@@ -0,0 +1,31 @@
+/*Given an array of integers nums, return the number of good pairs.
+
+A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+
+
+
+Example 1:
+
+Input: nums = [1,2,3,1,1,3]
+Output: 4
+Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.*/
+
+public class NumnerOfGoodPairs {
+    public static void main(String[] args) {
+        int[]nums = {1,2,3,1,1,3};
+        System.out.println(solutio(nums));
+        int[]nums2 = {1,2,3};
+        System.out.println(solutio(nums2));
+    }
+    public static int solutio(int[] nums){
+        int ans = 0;
+        for(int i = 0; i<nums.length; i++){
+            for(int j = i+1; j<nums.length; j++){
+                if(nums[i] == nums[j]){
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+}
